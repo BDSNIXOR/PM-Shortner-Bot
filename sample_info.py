fe7522fd2c885e8daa971394eca8ffb2cdaf310f
BDSNIXOR <149468089+BDSNIXOR@users.noreply.github.com>
2023-11-07 02:07:00 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
-USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+USER_SESSION = 'Nixor_Movies_Downloader_bot'
+API_ID = 25698401
+API_HASH = '1dcc0ddb25cfda941a4e046f4fedaf75'
+BOT_TOKEN = '6822768678:AAEuTOdR-IzrNR2Li0ujgx4hlUWX6q62pCQ'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -12,7 +12,7 @@ USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+CHANNELS = [-10012345678, -100987654321, 'nixor_movies_group']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
